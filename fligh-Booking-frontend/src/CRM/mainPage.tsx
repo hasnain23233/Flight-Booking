bb92cff98d9bd5833a63674bf5d5f28a06df7291
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-02-23 00:01:12 +0500

    working on the upper navbar

--- fligh-Booking-frontend/src/CRM/mainPage.tsx
+++ fligh-Booking-frontend/src/CRM/mainPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Routes, Route } from "react-router";
+import Dashboard from '../Dashboard/page';
+import Navbar from '../components/Navbar'
+import UpperNav from '../components/UpperNav';
 
 const mainPage = () => {
   return (
-    <div>
-      This is a main page routing
-    </div>
+    <>
+      <UpperNav />
+      <div className='flex'>
+        <div className='w-2/12 border'>
+          <Navbar />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ fligh-Booking-frontend/src/Dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Thsi is a Dashboard of the website
+    </div>
+  )
+}
+
+export default page
