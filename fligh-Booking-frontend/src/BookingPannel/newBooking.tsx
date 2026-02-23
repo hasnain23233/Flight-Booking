8b93f338424634b44d083c4b812577234df4a43b
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-02-24 02:59:53 +0500

    working on the booking page

--- fligh-Booking-frontend/src/BookingPannel/newBooking.tsx
+++ fligh-Booking-frontend/src/BookingPannel/newBooking.tsx
@@ -2,7 +2,7 @@ import BookingForm from "../components/forms/bookingForm"
 const newBooking = () => {
   return (
     <div className="w-full font-sans">
-        <div className=" mt-4">
+        <div className=" my-4">
           <h1 className='font-semibold text-3xl text-gray-800'>New Booking</h1>
           <p className='text-gray-700 text-lg leading-loose tracking-wider'>Fill in the details below to create a new booking</p>
         </div>
