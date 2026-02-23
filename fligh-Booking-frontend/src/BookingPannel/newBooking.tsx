031102901c7e612fbeb2cc9c2e479add36d135d4
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-02-24 00:51:37 +0500

    working on the booking pannel

---
+++ fligh-Booking-frontend/src/BookingPannel/newBooking.tsx
@@ -0,0 +1,16 @@
+import BookingForm from "../components/forms/bookingForm"
+const newBooking = () => {
+  return (
+    <div className="w-full font-sans">
+        <div className=" mt-4">
+          <h1 className='font-semibold text-3xl text-gray-800'>New Booking</h1>
+          <p className='text-gray-700 text-lg leading-loose tracking-wider'>Fill in the details below to create a new booking</p>
+        </div>
+        <div>
+            <BookingForm/>
+        </div>
+    </div>
+  )
+}
+
+export default newBooking
